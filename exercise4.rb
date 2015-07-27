f6b3f5859f999b1b628947bc084c5bb4ac459228
Sam Xu <sam.xu@utoronto.ca>
2015-07-27 17:28:14 -0400

    Create exercise4.rb

---
+++ exercise4.rb
@@ -0,0 +1,12 @@
+one_to_hundred = (1..100)
+
+one_to_hundred.each do |num|
+	if (num % 3 == 0)
+		puts "Bit"
+	elsif (num % 5 == 0)
+		puts "Maker"
+	elsif ((num % 3 == 0) || (num % 5 == 0))
+		puts "Bit Maker"
+	else
+		puts num
+end
